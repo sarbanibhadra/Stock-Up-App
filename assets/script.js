3bd75d1879f60c7f6b784cd17d28cb14d6a43326
Sarbani Das <sarbanidas@sarbanis-air.home>
2024-02-01 21:00:43 +0000

    first commit

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,88 @@ const poly_api_key= "BGrqA9jp0bOXCmaiGwDF8i6EACuuN7SD"
 
 const fetchRequest= "//api.polygon.io/v3/reference/tickers?search=" + companyName+ "&apiKey=" + poly_api_key;
 console.log(fetchRequest);
+
+
+
+/**
+ * pulls information from the form and build the query URL
+ * @returns {string} URL for Weather Dashboard API based on form inputs
+ */
+function buildQueryURL(input) {
+  // queryURL is the url we'll use to query the API
+  var queryURL = "";
+  
+  return queryURL;
+}
+
+
+/**
+ * takes API data (JSON/object) and turns it into elements on the page
+ * @param {object} stockData - object containing Weather Dashboard API data
+ */
+function updatePage(stockData) {
+}
+
+// CLICK HANDLERS
+// ==========================================================
+
+// .on("click") function associated with the Search Button
+$("#btn-primary").on("click", function (event) {
+  // This line allows us to take advantage of the HTML "submit" property
+  // This way we can hit enter on the keyboard and it registers the search
+  // (in addition to clicks). Prevents the page from reloading on form submit.
+  event.preventDefault();
+
+  input = $("#search-input")
+    .val()
+    .trim();
+
+  // Build the query URL for the Fetch request to the Weather Dashboard API
+  var queryURL = buildQueryURL(input);
+
+  // Make the Fetch request to the API - GETs the JSON data at the queryURL.
+  // The data then gets passed as an argument to the updatePage function
+  fetch(queryURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(updatePage);
+});
+
+// CLICK HANDLERS
+// ==========================================================
+
+// .on("click") function associated with the Search Button
+$("#btn-secondary").on("click", function (event) {
+  // This line allows us to take advantage of the HTML "submit" property
+  // This way we can hit enter on the keyboard and it registers the search
+  // (in addition to clicks). Prevents the page from reloading on form submit.
+  event.preventDefault();
+
+  // input will be fetched from storage data
+  input = "Need to fetch from storage data"
+
+  // Build the query URL for the Fetch request to the Weather Dashboard API
+  var queryURL = buildQueryURL(input);
+
+  // Make the Fetch request to the API - GETs the JSON data at the queryURL.
+  // The data then gets passed as an argument to the updatePage function
+  fetch(queryURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(updatePage);
+});
+
+function pageOnLoad(input){
+  // Build the query URL for the Fetch request to the NYT API
+  var queryURL = buildQueryURL(input);
+
+  // Make the Fetch request to the API - GETs the JSON data at the queryURL.
+  // The data then gets passed as an argument to the updatePage function
+  fetch(queryURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(updatePage);
+}
