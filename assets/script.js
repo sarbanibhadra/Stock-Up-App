7c34bfb910f373b8704fa4b1ef7131cd33a59e10
Yaseen fehad <yaseenfehad@gmail.com>
2024-02-01 18:37:52 +0000

    added both fetch requestes required and tested with console.log addded jquery and datepicker for updated html and css.

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 const apiKey = 'cmsltq9r01qpvcpt777gcmsltq9r01qpvcpt7780';
 const companysybol = 'AAPL'; //WE WANT THIS TO BE INPUT BY USER
-const companyName="Apple Inc" //polygone to get this?
+const companyName="TESLA" //polygone to get this?
 
 
 fetch("https://finnhub.io/api/v1/quote?symbol=" + companysybol + "&token=" + apiKey)
@@ -16,3 +16,15 @@ const poly_api_key= "BGrqA9jp0bOXCmaiGwDF8i6EACuuN7SD"
 
 const fetchRequest= "//api.polygon.io/v3/reference/tickers?search=" + companyName+ "&apiKey=" + poly_api_key;
 console.log(fetchRequest);
+fetch(fetchRequest)
+.then(response => response.json())
+  .then(companyData => {
+    console.log(companyData);
+  })
+  .catch(error => {
+    console.error('Error fetching company data:', error);
+  });
+
+  $( function() {
+    $( "#datepicker" ).datepicker();
+} );
