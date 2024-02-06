40d0fbb181d135e44192539552593375d40a5bf5
SpecialFriendRice <corinneborgeaud@hotmail.com>
2024-02-06 20:49:04 +0000

    html symbol values and js functionality update

--- assets/script.js
+++ assets/script.js
@@ -23,18 +23,23 @@ console.log(fetchRequest);
  * pulls information from the form and build the query URL
  * @returns {string} URL for the API based on form inputs
  */
-function buildQueryURL(input) {
+function buildQueryURL(companysybol) {
 
-  var companysybol = "AAPL";
+  // var companysybol = "AAPL";
   var apiKey= "cmv4ch1r01qog1iu9gogcmv4ch1r01qog1iu9gp0"
 
   // queryURL is the url we'll use to query the API
   //var queryURL = "https://finnhub.io/api/v1//search?q=apple"  + "&token=" + apiKey;
 
-  var queryURL = "https://finnhub.io/api/v1/quote?symbol=" + companysybol + "&token=" + apiKey;
+  var queryURL = "https://finnhub.io/api/v1/quote?symbol=" + companysybol + "&token=" + apiKey2;
   
   return queryURL;
 }
+
+
+//Second API call for historic stock data - needs to linked to an onchange event for datepicker
+// Shall we re-write the buildQueryURL function above to generate both API calls? Otherwise, second API call is as below:
+var queryURL2 = "https://api.polygon.io/v1/open-close/" + companysybol + "/" + chosenDate + "?adjusted=true&apiKey=" + apiKey2;
 
 
 /**
@@ -63,16 +68,19 @@ function updatePage(stockData) {
 // ==========================================================
 
 // .on("click") function associated with the Search Button
-$("#btn-primary").on("click", function (event) {
+// $("#btn-primary").on("click", function (event) {
+  $("#stocks").on("change", function (event) {
+    // DEVELOP: local storage for every stock picked and add to an array to make a fancy "stocks you looked at" card/div
   // This line allows us to take advantage of the HTML "submit" property
   // This way we can hit enter on the keyboard and it registers the search
   // (in addition to clicks). Prevents the page from reloading on form submit.
   event.preventDefault();
 
-  input = $("#search-input")
-    .val()
-    .trim();
-
+  // input = $("#search-input")
+  //   .val()
+  //   .trim();
+  input = $(event.target).val();
+console.log(input);
   // Build the query URL for the Fetch request to the API
   var queryURL = buildQueryURL(input);
 
@@ -89,7 +97,7 @@ $("#btn-primary").on("click", function (event) {
 // ==========================================================
 
 // .on("click") function associated with the Search Button
-$("#btn-secondary").on("click", function (event) {
+  $("#btn-secondary").on("click", function (event) {
   // This line allows us to take advantage of the HTML "submit" property
   // This way we can hit enter on the keyboard and it registers the search
   // (in addition to clicks). Prevents the page from reloading on form submit.
@@ -130,5 +138,9 @@ $( function() {
   $( "#datepicker" ).datepicker({
     dateFormat: "yy-mm-dd" // NOTE this actually returns yyyy-mm-dd format
     }
-  );
+  ); 
+  $("#datepicker" ).on("change", function(event){
+    console.log($(event.target).val());
+    var chosenDate = $(event.target).val();
+  })
 } );
